d2e7899a1b2ace4c408ece15472c0529b754eede
Breno <breno.nog.araujo@hotmail.com>
2021-10-19 20:19:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactHlsPlayer from "react-hls-player";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactHlsPlayer
+        src="https://newedge.eu-central-1.edge.mycdn.live/live/sportv224ha/playlist.m3u8"
+        autoPlay={true}
+        controls={true}
+        width="50%"
+        height="auto"
+      />
     </div>
   );
 }
